9b0dbfd71acc823608464557e6ae14dae828fc55
AssortedMine70 <fede.fred.05@gmail.com>
2024-04-17 19:26:01 +0200

    frontend + server fixes

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+let status = 'idle';
+
+app.get('/', (_, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/getStatus', (_, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(status);
+});
+
+app.get('/setStatus', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if(req.query.status >= 1 || req.query.status <= 7 || req.query.status == 'idle') {
+        status = req.query.status;
+        res.send('Status updated');
+        return;
+    }
+    res.send('Invalid status');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
